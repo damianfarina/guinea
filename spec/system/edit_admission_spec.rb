1d0e4e0aec1b1f0f934371c1b505f63231b17814
Damián Farina <damianfarina@gmail.com>
2018-02-03 01:10:53 -0300

    Edit admission checked exams (#18)

---
+++ spec/system/edit_admission_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe 'editing an admission', type: :system do
+  let(:veterinary) { create :veterinary }
+  let(:veterinarian) { create :veterinarian, veterinaries: [veterinary] }
+  let(:admission) do
+    create :admission,
+      veterinary: veterinary,
+      veterinarian: veterinarian,
+      petitioner_name: "#{veterinarian.full_name} (#{veterinary.name})",
+      petitioner_email: veterinarian.email,
+      petitioner_phone: veterinarian.phone,
+      exams: ['Urianálisis', 'Relación P:C', 'Análisis Cálculos Urinarios', 'Raspaje de Piel', 'Test Dermatofitos']
+  end
+
+  before(:each) do
+    visit edit_admission_path(admission)
+  end
+
+  it 'should display the form', js: true do
+    within 'fieldset.petitioner' do
+      expect(page).to have_select('veterinaria', with_selected: veterinary.name)
+      expect(page).to have_select('veterinario', with_selected: veterinarian.full_name)
+      expect(page).to have_field('nombre', with: "#{veterinarian.full_name} (#{veterinary.name})")
+      expect(page).to have_field('teléfono', with: veterinarian.phone)
+      expect(page).to have_field('EMail', with: veterinarian.email)
+    end
+
+    within 'fieldset.exams' do
+      expect(page).to have_field('Orina', checked: true)
+      expect(page).to have_field('Urianálisis', checked: true)
+      expect(page).to have_field('Relación P:C', checked: true)
+      expect(page).to have_field('Análisis Cálculos Urinarios', checked: true)
+      expect(page).to have_field('Piel', checked: true)
+      expect(page).to have_field('Raspaje de Piel', checked: true)
+      expect(page).to have_field('Test Dermatofitos', checked: true)
+
+      expect(page).to have_field('Urea', checked: false)
+      expect(page).to have_field('Coagulación', checked: false)
+    end
+
+    within 'fieldset.exams' do
+      uncheck 'Orina'
+      uncheck 'Piel'
+      check 'Urea'
+      check 'Coagulación'
+    end
+
+    click_on('actualizar orden de ingreso')
+
+    expect(page).to have_content('actualizado correctamente')
+    expect(page).to have_content("#{veterinarian.full_name} (#{veterinary.name})")
+    expect(page).to have_content(veterinarian.phone)
+    expect(page).to have_content(veterinarian.email)
+    expect(page).to have_content('Urea')
+    expect(page).to have_content('Coagulación')
+    expect(page).not_to have_content('Orina')
+    expect(page).not_to have_content('Urianálisis')
+    expect(page).not_to have_content('Relación P:C')
+    expect(page).not_to have_content('Análisis Cálculos Urinarios')
+    expect(page).not_to have_content('Piel')
+    expect(page).not_to have_content('Raspaje de Piel')
+    expect(page).not_to have_content('Test Dermatofitos')
+  end
+end
--- spec/views/admissions/show.html.erb_spec.rb
+++ spec/views/admissions/show.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'admissions/show', type: :view do
   let(:veterinary) { create :veterinary }
   let(:veterinarian) { create :veterinarian, veterinaries: [veterinary] }
+  let(:exams) { }
 
   before(:each) do
     @admission = assign(:admission, Admission.create!(
@@ -18,26 +19,39 @@ RSpec.describe 'admissions/show', type: :view do
       age: '5m',
       owner_name: 'Zorro',
       comments: 'Sargento',
-      exams: %w[Urea Hepatozoon]
+      exams: exams
     ))
   end
 
-  it 'renders admission attributes' do
-    render
-    expect(rendered).to have_selector('a', text: veterinarian.full_name)
-    expect(rendered).to have_selector('a', text: veterinary.name)
-    expect(rendered).to match(/#{veterinarian.phone}/)
-    expect(rendered).to match(/#{veterinarian.email}/)
-    expect(rendered).to match(/Patient Name/)
-    expect(rendered).to match(/equino/)
-    expect(rendered).to match(/femenino/)
-    expect(rendered).to match(/Black/)
-    expect(rendered).to match(/5m/)
-    expect(rendered).to match(/Zorro/)
-    expect(rendered).to match(/Sargento/)
-    expect(rendered).to match(/Bioquímica/)
-    expect(rendered).to match(/Urea/)
-    expect(rendered).to match(/Hemoparásitos/)
-    expect(rendered).to match(/Hepatozoon/)
+  describe 'with exams' do
+    let(:exams) { %w[Urea Hepatozoon] }
+
+    it 'renders admission attributes' do
+      render
+      expect(rendered).to have_selector('a', text: veterinarian.full_name)
+      expect(rendered).to have_selector('a', text: veterinary.name)
+      expect(rendered).to match(/#{veterinarian.phone}/)
+      expect(rendered).to match(/#{veterinarian.email}/)
+      expect(rendered).to match(/Patient Name/)
+      expect(rendered).to match(/equino/)
+      expect(rendered).to match(/femenino/)
+      expect(rendered).to match(/Black/)
+      expect(rendered).to match(/5m/)
+      expect(rendered).to match(/Zorro/)
+      expect(rendered).to match(/Sargento/)
+      expect(rendered).to match(/Bioquímica/)
+      expect(rendered).to match(/Urea/)
+      expect(rendered).to match(/Hemoparásitos/)
+      expect(rendered).to match(/Hepatozoon/)
+    end
+  end
+
+  describe 'without exams' do
+    let(:exams) { }
+
+    it 'renders admission attributes' do
+      render
+      expect(rendered).to have_selector('.exams .list > *', count: 0)
+    end
   end
 end
